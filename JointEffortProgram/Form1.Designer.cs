57e9c05deb95075de844978daa202705cb7522eb
Tyler Goodfred <tmgoodfred@gmail.com>
2022-08-16 18:29:10 -0500

    Initial Commit - currently reads/creates file that shows current date/time

--- JointEffortProgram/Form1.Designer.cs
+++ JointEffortProgram/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.refreshFileBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Location = new System.Drawing.Point(290, 83);
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.Size = new System.Drawing.Size(185, 23);
+            this.outputTxt.TabIndex = 0;
+            // 
+            // refreshFileBtn
+            // 
+            this.refreshFileBtn.Location = new System.Drawing.Point(366, 252);
+            this.refreshFileBtn.Name = "refreshFileBtn";
+            this.refreshFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshFileBtn.TabIndex = 1;
+            this.refreshFileBtn.Text = "Refresh";
+            this.refreshFileBtn.UseVisualStyleBackColor = true;
+            this.refreshFileBtn.Click += new System.EventHandler(this.refreshFileBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.refreshFileBtn);
+            this.Controls.Add(this.outputTxt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox outputTxt;
+        private Button refreshFileBtn;
     }
 }
