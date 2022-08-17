f5e72715541243937f1fe283eb32ac9e2c003c14
Tyler Goodfred <tmgoodfred@gmail.com>
2022-08-16 19:04:18 -0500

    Work for edward updated

--- JointEffortProgram/Form1.Designer.cs
+++ JointEffortProgram/Form1.Designer.cs
@@ -30,30 +30,91 @@
         {
             this.outputTxt = new System.Windows.Forms.TextBox();
             this.refreshFileBtn = new System.Windows.Forms.Button();
+            this.noteEntryTxt = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.blankSlateBtn = new System.Windows.Forms.Button();
+            this.refreshDataBtn = new System.Windows.Forms.Button();
+            this.previousEntryBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // outputTxt
             // 
-            this.outputTxt.Location = new System.Drawing.Point(290, 83);
+            this.outputTxt.Location = new System.Drawing.Point(298, 12);
             this.outputTxt.Name = "outputTxt";
+            this.outputTxt.ReadOnly = true;
             this.outputTxt.Size = new System.Drawing.Size(185, 23);
             this.outputTxt.TabIndex = 0;
+            this.outputTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // refreshFileBtn
             // 
-            this.refreshFileBtn.Location = new System.Drawing.Point(366, 252);
+            this.refreshFileBtn.Location = new System.Drawing.Point(681, 12);
             this.refreshFileBtn.Name = "refreshFileBtn";
-            this.refreshFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshFileBtn.Size = new System.Drawing.Size(107, 23);
             this.refreshFileBtn.TabIndex = 1;
-            this.refreshFileBtn.Text = "Refresh";
+            this.refreshFileBtn.Text = "Clear File Data";
             this.refreshFileBtn.UseVisualStyleBackColor = true;
             this.refreshFileBtn.Click += new System.EventHandler(this.refreshFileBtn_Click);
+            // 
+            // noteEntryTxt
+            // 
+            this.noteEntryTxt.Location = new System.Drawing.Point(157, 63);
+            this.noteEntryTxt.Multiline = true;
+            this.noteEntryTxt.Name = "noteEntryTxt";
+            this.noteEntryTxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.noteEntryTxt.Size = new System.Drawing.Size(490, 307);
+            this.noteEntryTxt.TabIndex = 2;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(363, 393);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 3;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // blankSlateBtn
+            // 
+            this.blankSlateBtn.Location = new System.Drawing.Point(505, 393);
+            this.blankSlateBtn.Name = "blankSlateBtn";
+            this.blankSlateBtn.Size = new System.Drawing.Size(75, 23);
+            this.blankSlateBtn.TabIndex = 4;
+            this.blankSlateBtn.Text = "Blank Slate";
+            this.blankSlateBtn.UseVisualStyleBackColor = true;
+            this.blankSlateBtn.Click += new System.EventHandler(this.blankSlateBtn_Click);
+            // 
+            // refreshDataBtn
+            // 
+            this.refreshDataBtn.Location = new System.Drawing.Point(681, 41);
+            this.refreshDataBtn.Name = "refreshDataBtn";
+            this.refreshDataBtn.Size = new System.Drawing.Size(107, 23);
+            this.refreshDataBtn.TabIndex = 5;
+            this.refreshDataBtn.Text = "Refresh To Latest";
+            this.refreshDataBtn.UseVisualStyleBackColor = true;
+            this.refreshDataBtn.Click += new System.EventHandler(this.refreshDataBtn_Click);
+            // 
+            // previousEntryBtn
+            // 
+            this.previousEntryBtn.Location = new System.Drawing.Point(200, 393);
+            this.previousEntryBtn.Name = "previousEntryBtn";
+            this.previousEntryBtn.Size = new System.Drawing.Size(94, 23);
+            this.previousEntryBtn.TabIndex = 6;
+            this.previousEntryBtn.Text = "Previous Entry";
+            this.previousEntryBtn.UseVisualStyleBackColor = true;
+            this.previousEntryBtn.Click += new System.EventHandler(this.previousEntryBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.previousEntryBtn);
+            this.Controls.Add(this.refreshDataBtn);
+            this.Controls.Add(this.blankSlateBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.noteEntryTxt);
             this.Controls.Add(this.refreshFileBtn);
             this.Controls.Add(this.outputTxt);
             this.Name = "Form1";
@@ -67,5 +128,10 @@
 
         private TextBox outputTxt;
         private Button refreshFileBtn;
+        private TextBox noteEntryTxt;
+        private Button submitBtn;
+        private Button blankSlateBtn;
+        private Button refreshDataBtn;
+        private Button previousEntryBtn;
     }
 }
--- JointEffortProgram/Form1.cs
+++ JointEffortProgram/Form1.cs
@@ -37,9 +37,10 @@ namespace JointEffortProgram
         {
             DateTime todaysDate = DateTime.Now; //this is a DateTime variable, this specifically handes dates and times. It has a method called "Now" that gets the current date and time.
             string text = todaysDate.ToString();    //this sets a string to the DateTime variable, but you can't just set todaysDate to a string, you have to convert it to a string. This is done with the method ".ToString();"
-            if (new FileInfo("date.txt").Length == 0)   //this will check to see if a file is empty, if it is, we want to create a new file. If not, we only want to read the info from it, not replace it.
+
+            if (new FileInfo("date.txt").Length <= 2)   //this will check to see if a file is empty, if it is, we want to create a new file. If not, we only want to read the info from it, not replace it.
             {
-                await File.WriteAllTextAsync("date.txt", text); //await works with async, it's not needed if you aren't doing async. But this is creating a new file called date.txt and putting the string into it.
+                await File.WriteAllTextAsync("date.txt", text + Environment.NewLine); //await works with async, it's not needed if you aren't doing async. But this is creating a new file called date.txt and putting the string into it.
                 //the location of this file will be: "C:\Users\***YOURUSERNAMEHERE***\source\repos\JointEffortProgram\JointEffortProgram\bin\Debug\net6.0-windows\date.txt"
                 currentDate = todaysDate; //here I am setting the variable currentDate to the date we just got from the method for consistency.
                 outputTxt.Text = currentDate.ToString();
@@ -47,17 +48,69 @@ namespace JointEffortProgram
             else
             {
                 //read the file info, set it to whatever the last date in the file is.
-                string lineToParse = File.ReadLines("date.txt").First();    //this takes the first line in the file and then we out it to a string
-                currentDate = DateTime.Parse(lineToParse);  //this converts the string to a DateTime and then sets it to our current variable
-                outputTxt.Text = currentDate.ToString();
-            }
+                var data = File.ReadAllLines("date.txt");       //this is an array of all the lines in the text file
 
+                string dateTime = data[data.Length - 1];    //this is the line that has the date time
+                string dataEntry = data[data.Length - 2];   //this is the line that has the entry from the text box
+
+                currentDate = DateTime.Parse(dateTime);  //this converts the string to a DateTime and then sets it to our current variable
+                noteEntryTxt.Text = dataEntry;  //this sets the info in the text box to the latest entry in the file
+                outputTxt.Text = currentDate.ToString();    //this sets the date time text box to the most recent time
+            }
         }
 
         private void refreshFileBtn_Click(object sender, EventArgs e) //this is the action event for when you press the clearfile button
         {
             File.Create("date.txt").Close();    //this clears the contents of the file by overwritting the existing one
-            CreateAndUpdateFile();  //I call the CreateAndUpdateFile method to take our new empty file and get the most up to date time on it.
+            //CreateAndUpdateFile();  //I call the CreateAndUpdateFile method to take our new empty file and get the most up to date time on it.
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            //here we want to save the entry text to the file
+            if (noteEntryTxt.Text != "" || noteEntryTxt.Text != null)    //here we want to check to make sure the user didn't just put nothing in.
+            {
+                string textOutput = noteEntryTxt.Text;
+                DateTime todaysDate = DateTime.Now;
+                File.AppendAllText("date.txt", textOutput + Environment.NewLine);
+                File.AppendAllText("date.txt", todaysDate.ToString() + Environment.NewLine);
+            }
+            else
+            {
+                noteEntryTxt.Text = "Put something here!";
+            }
+        }
+
+        private void blankSlateBtn_Click(object sender, EventArgs e)
+        {
+            noteEntryTxt.Text = ""; //this just sets the text box to empty for the user
+        }
+
+        private void refreshDataBtn_Click(object sender, EventArgs e)
+        {
+            var data = File.ReadAllLines("date.txt");       //this is an array of all the lines in the text file
+
+            string dateTime = data[data.Length - 1];    //this is the line that has the date time
+            string dataEntry = data[data.Length - 2];   //this is the line that has the entry from the text box
+
+            currentDate = DateTime.Parse(dateTime);  //this converts the string to a DateTime and then sets it to our current variable
+            noteEntryTxt.Text = dataEntry;  //this sets the info in the text box to the latest entry in the file
+            outputTxt.Text = currentDate.ToString();    //this sets the date time text box to the most recent time
+        }
+
+        private void previousEntryBtn_Click(object sender, EventArgs e)
+        {
+            //Edward, this is what I want you to work on.
+            /*
+             * What should happen here is that when you press the button it should go to the last entry in the file IF IT EXISTS.
+             * so my hints as to what to do are as follows:
+             * make an if/else statement to check to see if the file has more than 5 lines of text.
+             * The reason it's 5 is because in the file the first thing that should be there is the DateTime of the file creation.
+             * The next 2 items come as a pair and that's the text entry plus the date time of that text entry.
+             * so whenever you add an entry, it adds 2 lines. If we're going back into the past, we want at least 2 entries, so those plus the initial line are 5 total.
+             * 
+             * if there's more than 5, take the line data from the strings and output it!
+             */
         }
     }
 }
